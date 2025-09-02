be5aaf76ff69f2317fe084a5fd62d29f93868e50
MazharRehan <mazhar.rehan@invobyte.com>
2025-09-02 18:45:29 +0500

    perform CRUD operations

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -60,4 +60,39 @@ app.MapPost("games", (CreateGameDto newGame) =>
     return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
 });
 
+// PUT /games/1
+app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
+{
+    var game = games.Find(g => g.Id == id);
+    if (game is null)
+    {
+        return Results.NotFound();
+    }
+
+    var index = games.FindIndex(game => game.Id == id);
+
+    games[index] = new GameDto(
+         id,
+         updatedGame.Name,
+         updatedGame.Genre,
+         updatedGame.Price,
+         updatedGame.ReleaseDate);
+
+    return Results.NoContent();
+});
+
+// DELETE /games/1
+app.MapDelete("games/{id}", (int id) =>
+{
+    var game = games.Find(g => g.Id == id);
+    if (game is null)
+    {
+        return Results.NotFound();
+    }
+
+    games.RemoveAll(game => game.Id == id);
+
+    return Results.NoContent();
+});
+
 app.Run();
